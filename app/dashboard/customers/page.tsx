e0e5b4a108cc076592679a096330634c20acd361
bitbyte29 <4gaminghsp@gmail.com>
2024-04-28 22:46:45 +0530

    initial commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+const Customer = () => {
+  return <>This is costumer page on dashboard</>;
+};
+
+export default Customer;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+const Invoices = () => {
+  return <>THis is incoices page</>;
+};
+
+export default Invoices;
